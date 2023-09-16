c47809bc52bafd0276e00be94258d652d3681530
NikitaPos <138600818+NikitaPos@users.noreply.github.com>
2023-09-16 17:30:07 +0000

    first release

---
+++ fernet-os/fernet-os/Kernel.cs
@@ -0,0 +1,163 @@
+﻿using Cosmos.System.FileSystem;
+using Cosmos.System.FileSystem.VFS;
+using Cosmos.System.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Sys = Cosmos.System;
+using System.IO;
+
+namespace fernet_os
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome to fernet OS. Type commandslist to see all commands");
+            Console.WriteLine("||||||||||||||||||||||");
+            Console.WriteLine("|                    |");
+            Console.WriteLine("|                    |");
+            Console.WriteLine("|      fernet_os     |");
+            Console.WriteLine("|                    |");
+            Console.WriteLine("|                    |");
+            Console.WriteLine("||||||||||||||||||||||");
+            File.Create("0:\readme.txt");
+            File.WriteAllText("0:\test.txt", "Ferner_os is os made by NikitaPos");
+            try
+            {
+                VFSManager.RegisterVFS(fs);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Filesystem initialization failed!");
+                Console.ReadKey();
+                Cosmos.System.Power.Shutdown();
+            }
+        }
+        public static CosmosVFS fs = new();
+        public static bool gui = false;
+        public static Canvas guiDisplay;
+        protected override void Run()
+        {
+
+        shell:
+
+            Console.Write("fernet_os> ");
+            var input = Console.ReadLine();
+
+            if (input == "commandslist")
+
+            {
+
+                Console.WriteLine("launch - turn on visual mode");
+                Console.WriteLine("clear - clear console from text");
+                Console.WriteLine("color settings - color options");
+                Console.WriteLine("credits - credits");
+
+            }
+
+            if (input == "clear")
+
+            {
+
+                Console.Clear();
+
+            }
+
+            if (input == "credits")
+
+            {
+
+                Console.WriteLine("Special thanks to PratyushKing");
+
+            }
+
+
+            if (input == "")
+
+            {
+
+                Console.WriteLine("That is not command");
+
+            }
+
+            if (input == "color settings")
+
+            {
+                Console.WriteLine("green - change color of text to green (console)");
+                Console.WriteLine("blue - change color of text to blue (console)");
+                Console.WriteLine("white - change color of text to white (console)");
+                Console.WriteLine("yellow - change color of text to orange (console)");
+                Console.WriteLine("cyan - change color of text to brown (console)");
+
+            }
+
+            if (input == "green")
+
+            {
+
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            }
+
+            if (input == "blue")
+
+            {
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+            }
+
+            if (input == "white")
+
+            {
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+
+            if (input == "yellow")
+
+            {
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+            }
+
+            if (input == "cyan")
+
+            {
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+            }
+
+            if (!gui)
+            {
+                
+                if (input == "launch")
+                {
+                    gui = true;
+                    Sys.MouseManager.ScreenWidth = 640;
+                    Sys.MouseManager.ScreenHeight = 480;
+                    var x = 640;
+                    var y = 480;
+                    guiDisplay = FullScreenCanvas.GetFullScreenCanvas(new Mode(640, 480, ColorDepth.ColorDepth32));
+                    var yourcolor = Color.MediumPurple;
+                    guiDisplay.Clear(yourcolor);
+                    guiDisplay.DrawFilledCircle(Color.White, (int)Sys.MouseManager.X, (int)Sys.MouseManager.Y, 5);
+                    guiDisplay.Display();
+                }
+            }
+
+            else
+            {
+                
+            }
+            goto shell;
+        }
+    }
+}
